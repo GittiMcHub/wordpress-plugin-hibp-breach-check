e2f252b8fa3c8f495f8465a50018f4785983332f
DominikTobaben1061 <github-dt@tobaben.it>
2024-06-13 02:43:56 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,62 @@
+# Usage
+```
+[hibp_checker 
+  breach="Adobe" 
+  apikey="abcdefghijklmnop" 
+  toggle-breach="hibp-toggle-breach" 
+  toggle-safe="hibp-toggle-safe" 
+  input-placeholder="+49..."] 
+  <b>Jetzt Handynummer eingeben und kostenlos prüfen, ob Du betroffen bist:</b> 
+[/hibp_checker]
+```
+- Content inside the Shortcode will be placed above the Form.
+- If Data is found in given Breach, the HTML Element with ID "hibp-toggle-breach" will be visible or hidden (toggles).
+- If Data is NOT found in given Breach, the HTML Element with ID "hibp-toggle-safe" will be visible or hidden (toggles).
+
+# Styling
+The Generated code is the following
+```html 
+<div class="hibp-container">
+    <div class="hibp-content"> 
+        $content
+    </div>
+    <form id="hibp-form-$breach" action="current/url" method="post">
+        <input class="hibp-input" placeholder="$input-placeholder" name="hibp_input">
+        <input type="submit" class="hibp-button" name="submit" value="Jetzt Prüfen">
+    </form>
+</div>
+```
+You can add a custom CSS File via WPCode or other Code Injection Plugins to style this element.
+```css 
+.hibp-container {
+    padding-top: 0px !important;
+}
+
+.hibp-container form {
+	display: flex;
+	align-items: center;
+}
+
+.hibp-input {
+    border: solid black 4px !important;
+}
+
+.hibp-button {
+    cursor: pointer !important;
+    border: solid black 1px !important;
+    border-radius: 4px !important;
+    background-color: #black !important;
+    color: #fff !important;
+    padding-top: 8px !important;
+	padding-bottom: 8px !important;
+	padding-left: 18px !important;
+	padding-right: 18px !important;
+}
+
+#hibp-toggle-breach {
+	display: none;
+}
+#hibp-toggle-safe {
+	display: none;
+}
+```
---
+++ index.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * Plugin Name: HIBP Breach Checker
+ * Plugin URI: http://example.com
+ * Description: Provides Input Field for https://haveibeenpwned.com/ API v3/breachedaccount. If found, it will toogle an Elements visibility: [hibp_checker apikey="" breach="Facebook" toggle-safe="hibp-toggle-safe" toggle-breach="hibp-toggle-breach" input-placeholder="+49..." ]
+ * Version: 1.0
+ * Author: GittiMcHub
+ * Author URI: https://github.com/GittiMcHub/
+ */
+
+if (!defined('ABSPATH')) {
+	exit; // Exit if accessed directly
+}
+
+function user_form($breach, $input_placeholder, $content)
+{
+	$out  = '<div class="hibp-container">';
+	if ( $content !== null) {
+		$out .= '<div class="hibp-content">' . $content . '</div>';
+	}
+	$out .= '<form id="hibp-form-' . $breach . '" action="' . esc_url($_SERVER['REQUEST_URI']) . '" method="post">';
+	$out .= '<input class="hibp-input" placeholder="'. $input_placeholder .'" name="hibp_input" />';
+	$out .= '<input type="submit" class="hibp-button" name="submit" value="Jetzt Prüfen" />';
+	$out .= '</form></div>';
+	return $out;
+}
+
+function user_feedback($toggle_element_id)
+{
+	$out = "";
+	if ( $toggle_element_id !== null) {	
+		$out .= '<script type="text/javascript">';
+		$out .= "jQuery(document).ready(function() {
+				function toggleVisibility(elementId) {
+					var element = jQuery(\"#\" + elementId);
+					if (element.is(\":visible\")) {
+						element.hide();
+					} else {
+						element.show();
+					}
+				};";
+		$out .= "toggleVisibility('" . $toggle_element_id . "');";
+		$out .= "});";
+		$out .= "</script>";
+	}
+	return $out;
+}
+
+
+function hibp_api_call($apikey, $input)
+{
+	$url = 'https://haveibeenpwned.com/api/v3/breachedaccount/' . $input;
+	$headers = array(
+		'hibp-api-key' => $apikey,
+		'user-agent'   => 'HIBP Custom WordPress Plugin'
+	);
+	$response = wp_remote_get($url, array(
+		'headers' => $headers
+	));
+	if (is_wp_error($response)) {
+		throw new Exception('FEHLER: ' . esc_html($response->get_error_message()));
+	}
+	return json_decode(wp_remote_retrieve_body($response));
+}
+
+// Funktion zur Erstellung des Shortcodes
+function hibp_checker_shortcode($atts = [], $content = null, $tag = '')
+{
+	// normalize attribute keys, lowercase
+	$atts = array_change_key_case((array) $atts, CASE_LOWER);
+	// override default attributes with user attributes
+	$hibp_atts = shortcode_atts(
+		array(
+			'apikey' => '',
+			'breach' => 'Facebook',
+			'toggle-breach' => 'hibp-toggle-breach',
+			'toggle-safe' => 'hibp-toggle-safe',
+			'input-placeholder' => '+49...',
+		),
+		$atts,
+		$tag
+	);
+	// During a submit
+	if (isset($_POST['submit']) && isset($_POST['hibp_input'])) {
+		$user_input = sanitize_text_field($_POST['hibp_input']);
+		try {
+			// get reponse
+			$apiresponse = hibp_api_call($hibp_atts['apikey'], $user_input);
+			
+			if (!empty($apiresponse)) {
+				foreach ($apiresponse as $breach) {
+					if ($breach->Name === $hibp_atts['breach']) {
+						return user_feedback($hibp_atts['toggle-breach']);
+					}
+				}
+			}
+			// In case API Call fails
+		} catch (Exception $e) {
+			return "<p>" . $e->getMessage() .  "</p>";
+		}
+		return user_feedback($hibp_atts['toggle-safe']);
+	}
+	return user_form($hibp_atts['breach'], $hibp_atts['input-placeholder'], $content);
+}
+
+function hibp_shortcode_init()
+{
+	wp_enqueue_style('hibp-style', plugin_dir_url(__FILE__) . 'public/hibp.css', array(), "1.0", 'all');
+	wp_enqueue_script('jquery');
+	add_shortcode('hibp_checker', 'hibp_checker_shortcode');
+}
+
+
+add_action('init', 'hibp_shortcode_init');
